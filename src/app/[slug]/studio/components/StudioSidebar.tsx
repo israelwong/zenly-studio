562980cd3bda0f651940d0fd6fb91e05550674f1
Israel Wong <israelwong@icloud.com>
2025-11-23 17:25:23 -0600

    fix: limpiar import no usado de Link

--- src/app/[slug]/studio/components/StudioSidebar.tsx
+++ src/app/[slug]/studio/components/StudioSidebar.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React from 'react';
-import Link from 'next/link';
 import {
     ZenSidebar, ZenSidebarContent, ZenSidebarFooter, ZenSidebarMenu,
     ZenSidebarMenuItem, useZenSidebar
